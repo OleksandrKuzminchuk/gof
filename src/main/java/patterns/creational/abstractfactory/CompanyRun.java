41bc30ad64b993db152b46f9c2dae67837d779dd
Aleksandr Kuzminchuk <lgoptimusg252@gmail.com>
2023-02-07 21:53:11 +0200

    some refactoring Abstract Factory pattern

--- src/main/java/patterns/creational/abstractfactory/CompanyRun.java
+++ src/main/java/patterns/creational/abstractfactory/CompanyRun.java
@@ -1,17 +1,14 @@
 package patterns.creational.abstractfactory;
 
-import static patterns.creational.abstractfactory.CompanyAbstractCompany.getCompanyTeamFactoryByLanguage;
-import static patterns.creational.abstractfactory.TypeLanguage.RU;
+import static patterns.creational.abstractfactory.AbstractCompanyTeamFactory.getCompanyTeamFactoryByLanguage;
+import static patterns.creational.abstractfactory.TypeLanguage.EU;
 
 public class CompanyRun {
     public static void main(String[] args) {
-        CompanyTeamFactory productTeamFactory = getCompanyTeamFactoryByLanguage(RU);
-        Supplier supplier = productTeamFactory.getSupplier();
-        Employee employee = productTeamFactory.getEmployee();
-        Customer customer = productTeamFactory.getCustomer();
+        CompanyTeamFactory productTeamFactory = getCompanyTeamFactoryByLanguage(EU);
 
-        supplier.supplyProduct();
-        employee.makeJob();
-        customer.buyProduct();
+        productTeamFactory.getSupplier().supplyProduct();
+        productTeamFactory.getEmployee().makeJob();
+        productTeamFactory.getCustomer().buyProduct();
     }
 }

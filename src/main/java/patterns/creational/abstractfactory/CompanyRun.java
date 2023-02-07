52214c2139e3db714522a2db8f6601d38122ba64
Aleksandr Kuzminchuk <lgoptimusg252@gmail.com>
2023-02-07 20:14:32 +0200

    some refactoring Abstract Factory pattern

--- src/main/java/patterns/creational/abstractfactory/CompanyRun.java
+++ src/main/java/patterns/creational/abstractfactory/CompanyRun.java
@@ -1,11 +1,11 @@
 package patterns.creational.abstractfactory;
 
-import patterns.creational.abstractfactory.companyeu.CompanyTeamFactoryEU;
-import patterns.creational.abstractfactory.companyru.CompanyTeamFactoryRU;
+import static patterns.creational.abstractfactory.CompanyAbstractCompany.getCompanyTeamFactoryByLanguage;
+import static patterns.creational.abstractfactory.TypeLanguage.RU;
 
 public class CompanyRun {
     public static void main(String[] args) {
-        CompanyTeamFactory productTeamFactory = createCompanyTeamFactoryByLanguage(TypeLanguage.RU);
+        CompanyTeamFactory productTeamFactory = getCompanyTeamFactoryByLanguage(RU);
         Supplier supplier = productTeamFactory.getSupplier();
         Employee employee = productTeamFactory.getEmployee();
         Customer customer = productTeamFactory.getCustomer();
@@ -13,17 +13,5 @@ public class CompanyRun {
         supplier.supplyProduct();
         employee.makeJob();
         customer.buyProduct();
-    }
-
-    static CompanyTeamFactory createCompanyTeamFactoryByLanguage(TypeLanguage typeLanguage){
-        if (typeLanguage.name().equalsIgnoreCase(TypeLanguage.EU.name())){
-            System.out.println("Company crm system is running");
-            return new CompanyTeamFactoryEU();
-        } else if (typeLanguage.name().equalsIgnoreCase(TypeLanguage.RU.name())) {
-            System.out.println("Система управления компании запускается");
-            return new CompanyTeamFactoryRU();
-        }else {
-            throw new RuntimeException("Not support a language: " + typeLanguage.name());
-        }
     }
 }
